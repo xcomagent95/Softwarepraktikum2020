b4537f6302fc985ed374e980adebca41a4895b65
OTI2020 <61976211+OTI2020@users.noreply.github.com>
2020-03-13 16:51:45 +0100

    Refactor FloodFill und Kommentare in Georeference korrigiert

--- src/main/java/org/lakedetection/FloodFillAlgo.java
+++ src/main/java/org/lakedetection/FloodFillAlgo.java
@@ -3,41 +3,35 @@ package org.lakedetection;
 /**
  * author: OTI2020 && heilandoo
  * title: SoPraTestBand 
- * aim: testing and implementaion of floodfill-similar algorithem to detect connected areas of same color
- * date: 11.03.2020; 11:20 am 
+ * aim: count coherent Pixels of same color
+ * latest date: 11.03.2020; 11:20 am 
  **/
 
 public class FloodFillAlgo {
-	private static int px = 0;
-	private static int py = 0;
-	private static int anzPixel = 0;
-	private static float[][] ausgangsArr = {
-			{0  ,0  ,0  ,0  ,0  ,0  ,0  ,0  ,0  ,255},
-			{0  ,255,255,255,255,255,255,255,0  ,0  },
-			{0  ,255,255,255,255,255,255,255,255,0  },
-			{0  ,0  ,255,255,0  ,0  ,0  ,255,255,0  },
-			{0  ,255,255,255,0  ,255,0  ,255,255,0  },
-			{0  ,0  ,255,255,0  ,0  ,0  ,255,0  ,0  },
-			{0  ,0  ,255,255,255,255,255,0  ,0  ,0  },
-			{0  ,255,255,255,255,255,0  ,0  ,0  ,0  },
-			{0  ,0  ,0  ,255,255,255,0  ,0  ,255,255},
-			{255,0  ,0  ,0  ,0  ,0  ,0  ,255,255,255}};
+	private static int px;
+	private static int py;
+	private static int numPixel = 0;
+	private static float[][] inputArr;
 	
-	/*
+	
+	
 	//Kernstuek des Algor. ist die checkNeighbour()
-	public static void main(String[] args){
+	public static void calcAndPrintNumOfConnenctedPixel(float[][] inInputArr, int inPx, int inPy){
+		px = inPx;
+		py = inPy;
+		inputArr = inInputArr;
 		checkNeighbour(px, py);
-		presentPixelAnzahl();
+		presentPixelCount();
 		}
-	*/
-	
+		
 	//pixelCounter() setzt den Zaehler anzPixel bei jedem Aufruf einshöher,
 	//was aber nur passiert, wenn checkNeighbour() eine bisher ununtersuchte 
 	//Nachbarzelle mit dem Wert 255 aufspuert
-	public static int pixelCounter() {
-		anzPixel += 1;
+	private static int pixelCounter() {
+		numPixel += 1;
+		// Dies wird bei jedem neu gefundenem Pixel geprintet
 		//System.out.println("Die momentane Anzahl der zusammenhaengenden und untersuchten Pixel: " + anzPixel);
-		return anzPixel;
+		return numPixel;
 	}
 	
 	//checkNeighbour() fragt alle 8 Nachbarzellen des vorgegebenen Ausgangspixels, 	
@@ -51,91 +45,91 @@ public class FloodFillAlgo {
 	//auskommentiert werden(ebenso die else-Anweisungen). Die letzte Zeile der 
 	//jeweiligen if-Abfragen enthaelt den Rekursionsschritt, der den untersuchten 
 	//Nachbarpixel zum neuen Startpixel macht.
-	public static void checkNeighbour(float px_in, float py_in) {
-		if((py < (ausgangsArr[px].length - 1)) && (ausgangsArr[px][py + 1] == 255.0f)) {
+	private static void checkNeighbour(int inPx, int inPy) {
+		if((py < (inputArr[px].length - 1)) && (inputArr[px][py + 1] == 255.0f)) {
 			pixelCounter();
 			py += 1;
-			ausgangsArr[px][py] = 100.01f;
+			inputArr[px][py] = 100.01f;
 			//System.out.println(ausgangsArr[px][py]);
 			checkNeighbour(px, py);
 			py -= 1;
 		}//else System.err.println("unten");
 		
-		if((py < (ausgangsArr[px].length - 1)) && (px > 0) && (ausgangsArr[px - 1][py + 1] == 255.0f)) {
+		if((py < (inputArr[px].length - 1)) && (px > 0) && (inputArr[px - 1][py + 1] == 255.0f)) {
 			pixelCounter();
 			px -= 1;
 			py += 1;
-			ausgangsArr[px][py] = 100.02f;
+			inputArr[px][py] = 100.02f;
 			//System.out.println(ausgangsArr[px][py]);
 			checkNeighbour(px, py);
 			px += 1;
 			py -= 1;
 		}//else System.err.println("unten links");
 		
-		if((px > 0) && (ausgangsArr[px - 1][py] == 255.0f)) {
+		if((px > 0) && (inputArr[px - 1][py] == 255.0f)) {
 			pixelCounter();
 			px -= 1;
-			ausgangsArr[px][py] = 100.03f;
+			inputArr[px][py] = 100.03f;
 			//System.out.println(ausgangsArr[px][py]);
 			checkNeighbour(px, py);
 			px += 1;
 		}//else System.err.println("links");
 		
-		if((py > 0) && (px > 0) && (ausgangsArr[px - 1][py - 1] == 255.0f)) {
+		if((py > 0) && (px > 0) && (inputArr[px - 1][py - 1] == 255.0f)) {
 			pixelCounter();
 			px -= 1;
 			py -= 1;
-			ausgangsArr[px][py] = 100.04f;
+			inputArr[px][py] = 100.04f;
 			//System.out.println(ausgangsArr[px][py]);
 			checkNeighbour(px, py);
 			px += 1;
 			py += 1;
 		}//else System.err.println("oben links");
 
-		if((py > 0) && (ausgangsArr[px][py - 1] == 255.0f)) {
+		if((py > 0) && (inputArr[px][py - 1] == 255.0f)) {
 			pixelCounter();
 			py -= 1;
-			ausgangsArr[px][py] = 100.05f;
+			inputArr[px][py] = 100.05f;
 			//System.out.println(ausgangsArr[px][py]);
 			checkNeighbour(px, py);
 			py += 1;
 		}//else System.err.println("oben");
 
-		if((px < (ausgangsArr.length - 1)) && (py >0) && (ausgangsArr[px + 1][py - 1] == 255.0f)) {
+		if((px < (inputArr.length - 1)) && (py >0) && (inputArr[px + 1][py - 1] == 255.0f)) {
 			pixelCounter();
 			px += 1;
 			py -= 1;
-			ausgangsArr[px][py] = 100.06f;
+			inputArr[px][py] = 100.06f;
 			//System.out.println(ausgangsArr[px][py]);
 			checkNeighbour(px, py);
 			px -= 1;
 			py += 1;
 		}//else System.err.println("oben rechts");
 		
-		if((px < (ausgangsArr.length - 1)) && (ausgangsArr[px + 1][py] == 255.0f)) {
+		if((px < (inputArr.length - 1)) && (inputArr[px + 1][py] == 255.0f)) {
 			pixelCounter();
 			px += 1;
-			ausgangsArr[px][py] = 100.07f;
+			inputArr[px][py] = 100.07f;
 			//System.out.println(ausgangsArr[px][py]);
 			checkNeighbour(px, py);
 			px -= 1;
 		}//else System.err.println("rechts");
 
-		if((px < (ausgangsArr.length - 1)) && (py < (ausgangsArr[px].length - 1)) && (ausgangsArr[px + 1][py + 1] == 255.0f)) {
+		if((px < (inputArr.length - 1)) && (py < (inputArr[px].length - 1)) && (inputArr[px + 1][py + 1] == 255.0f)) {
 			pixelCounter();
 			px += 1;
 			py += 1;
-			ausgangsArr[px][py] = 100.08f;
+			inputArr[px][py] = 100.08f;
 			//System.out.println(ausgangsArr[px][py]);
 			checkNeighbour(px, py);
 			px -= 1;
 			py -= 1;
 		}//else System.err.println("unten rechts");
     }
-	public static void presentPixelAnzahl() {
+	private static void presentPixelCount() {
 		System.out.println("Die finale Anzahl der zusammenhaengenden und untersuchten Pixel mit ");
-		System.out.println("der urspruenglich abgespeicherten Gleitkommerzahl 255.0 belauft sich auf " + anzPixel + "  :)");
-		System.out.print("Die Wasserfläche hat damit eine angenäherte Oberfläche von " + anzPixel * 25 + " m^2");
+		System.out.println("der urspruenglich abgespeicherten Gleitkommerzahl 255.0 belaeuft sich auf " + numPixel + "  :)");
+		System.out.println("Die Wasserflaeche hat damit eine angenaeherte Oberflaeche von " + numPixel * 25 + " m^2");
 	}
 	
 }
--- src/main/java/org/lakedetection/Georeference.java
+++ src/main/java/org/lakedetection/Georeference.java
@@ -11,19 +11,19 @@ import org.esa.snap.core.datamodel.Product;
 public class Georeference {
 	
 	/* Das Ziel dieser Methode ist, dass die beiden zu vergleichenden Bilder georeferenziert und lokalisiert werden,
-	 * um sicherzustellen, dass es sich um dasselbe Gewässer handelt.
+	 * um sicherzustellen, dass es sich um dasselbe Gewaesser handelt.
 	 * Dazu wird im zuerst betrachteteten Bildausschnitt der Punkt (0,0) oben links als 
 	 * Ausgangspunkt betrachtet. 
-	 * Dann werden für diesen Punkt die Koordinaten abgefragt. 
+	 * Dann werden fuer diesen Punkt die Koordinaten abgefragt. 
 	 * 
-	 * Im Folgenden würde ein zweites Bild betrachtet werden und für die Koordinaten, die übergeben werden müssten,
-	 * würde nach den Koordinaten geschaut werden und, falls vorhanden, die Pixel-Position abgefragt werden.
+	 * Im Folgenden wuerde ein zweites Bild betrachtet werden und fuer die Koordinaten, die uebergeben werden muessten,
+	 * wuerde nach den Koordinaten geschaut werden und, falls vorhanden, die Pixel-Position abgefragt werden.
 	 * 
-	 * Nun ist von dem Ausgangspunkt (0,0) über die Koordinaten die Position dieses Punktes
-	 * im zweiten Bild ermittelt worden. Nun können diese Bilder verglichen werden. 
+	 * Nun ist von dem Ausgangspunkt (0,0) ueber die Koordinaten die Position dieses Punktes
+	 * im zweiten Bild ermittelt worden. Nun wuerden diese Bilder verglichen werden koennen. 
 	 * 
 	 * Ggf. ist es sinnvoll, zwei Punkte zu nehmen, einen (0,0) und den zweiten bspw. (0,10), damit die Bilder auch genordet bzw. ausgerichtet 
-	 * überlegt werden können.s
+	 * uebereinander gelegt werden koennen.s
 	 * */
 	
 	private String[] geo; // Erzeugt Array aus Strings names "geo" 
@@ -43,7 +43,7 @@ public class Georeference {
 		}
 		product.getTiePointGridNames();
 		
-		geo = product.getTiePointGridNames(); // Füllt das oben erzeugte Array mit dem Namen der GCP-Liste
+		geo = product.getTiePointGridNames(); // Faellt das oben erzeugte Array mit dem Namen der GCP-Liste
 	}
 
 	public GeoPos georef() { // MACHT ES SINN HIER "Product p" ZU UEBERGEBEN???
@@ -51,19 +51,14 @@ public class Georeference {
 		GeoPos geoPos = new GeoPos();//null//Bsp. GCP_20: (10.708067893981934d, 54.363765716552734d); // lat./long. // akt. auf "null" gesetzt, evtl.noch ergänzen
 		PixelPos pixelPos = new PixelPos(26472, 0); // Bsp. GCP_20
 
-		product.getTiePointGridGroup().get(product.getTiePointGridGroup().getName()); // gibt die Knoten der Gruppe zurück
+		product.getTiePointGridGroup().get(product.getTiePointGridGroup().getName()); // gibt die Knoten der Gruppe zurueck
 
 		// Bildpunkt oben links (0,0) ist Vergleichspunkt. Von dort werden die Koordinaten erfragt, damit im Vergleichsbild nach den Koordinaten gesucht werden kann. Z.B. mit product.getSceneGeoCoding().getPixelPos(geoPos, pixelPos);
 		PixelPos pixelPos2 = new PixelPos(0,0); 
 		
-		return product.getSceneGeoCoding().getGeoPos(pixelPos2, geoPos); // gibt Längen- und Breitengrad des GCP zurück
-		//product.getTiePointGridGroup().get(product.getTiePointGridGroup().getName()); // gibt die Knoten der Gruppe zurück
-		//product.getSceneGeoCoding().getPixelPos(geoPos, pixelPos); // Rückabfrage möglich?
-	}
-	
-	// Gibt den Namen der GCP-Punktliste zurück // Fkt. eig. nicht notw.
-	public String[] getName() {
-		return geo;
+		return product.getSceneGeoCoding().getGeoPos(pixelPos2, geoPos); // gibt Laengen- und Breitengrad des GCP zurueck
+		//product.getTiePointGridGroup().get(product.getTiePointGridGroup().getName()); // gibt die Knoten der Gruppe zurueck
+		//product.getSceneGeoCoding().getPixelPos(geoPos, pixelPos); // Rueckabfrage moeglich?
 	}
 	
 	// Vergleicht zwei Koordinaten miteinander
