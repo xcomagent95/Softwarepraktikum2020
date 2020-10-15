ba131b048a113b993460ba762e25502969de45c7
jbalzer12 <61976072+jbalzer12@users.noreply.github.com>
2020-10-15 10:21:46 +0200

    Kommentar

--- src/main/java/org/lakedetection/Georeference.java
+++ src/main/java/org/lakedetection/Georeference.java
@@ -10,7 +10,8 @@ import org.esa.snap.core.datamodel.Product;
 
 public class Georeference {
 	
-	/* Das Ziel dieser Methode ist, dass die beiden zu vergleichenden Bilder georeferenziert und lokalisiert werden,
+	/**
+	 * Das Ziel dieser Methode ist, dass die beiden zu vergleichenden Bilder georeferenziert und lokalisiert werden,
 	 * um sicherzustellen, dass es sich um dasselbe Gewaesser handelt.
 	 * Dazu wird im zuerst betrachteteten Bildausschnitt der Punkt (0,0) oben links als 
 	 * Ausgangspunkt betrachtet. 
@@ -24,33 +25,56 @@ public class Georeference {
 	 * 
 	 * Ggf. ist es sinnvoll, zwei Punkte zu nehmen, einen (0,0) und den zweiten bspw. (0,10), damit die Bilder auch genordet bzw. ausgerichtet 
 	 * uebereinander gelegt werden koennen.s
-	 * */
+	 */
 	
 	private String[] geo; // Erzeugt Array aus Strings names "geo" 
 	
-	private Product product;
+	private Product product; // Erzeugt ein Prdosukt vom Typp Product
 	
+	/**
+	 * Konstruktor der Klasse Georeference; Die Tabelle von Verbindungspunkten wird als String-Array 
+	 * gespeichert.
+	 * @param Ein Produkt, genannt p
+	 */
 	public Georeference(Product p) {
 		product = p;
 		geo = product.getTiePointGridNames(); // Fuellt das oben erzeugte Array mit dem Namen der GCP-Liste
 	}
-
-	//// Im Main festsetzen auf 11178 x 7116 für Mueritzsee und später im Gui den Nutzer auffordern selber eine Punkt einzugeben
-	public GeoPos getGeoPos(PixelPos p) {
-		return product.getSceneGeoCoding().getGeoPos(p, null);
+	
+	/**
+	 * Getter für die WGS84-Koordinaten
+	 * @param Zwei Doubles sind ein Pixelpaar
+	 * @return WGS84-Koordinaten des Pixelpaares
+	 */
+	public GeoPos getGeoPos(double x, double y) {
+		PixelPos pixpos = new PixelPos(x, y);
+		return product.getSceneGeoCoding().getGeoPos(pixpos, null);
 	}
 	
-	public PixelPos getPixPos(double x, double y) {//GeoPos p) {
-		GeoPos geopos = new GeoPos(x, y);
+	/**
+	 * Getter für die Pixel-Werte der Koordinaten aus dem Bild
+	 * @param Zwei Doubles die Längen- und Breitengrad sind
+	 * @return Pixelwerte des Koordinatenpunktes auf dem Bild
+	 */
+	public PixelPos getPixPos(double lat, double lon) {//GeoPos p) {
+		GeoPos geopos = new GeoPos(lat, lon);
 		return product.getSceneGeoCoding().getPixelPos(geopos, null);
 	}
 	
-	// Gibt den Namen der GCP-Punktliste zurück // Fkt. eig. nicht notw.
+	/**
+	 * Getter für den Namen der GCP-Punktliste
+	 * @return Namen der GCP-Punktliste
+	 */
 	public String[] getName() {
 		return geo;
 	}
 	
-	// Vergleicht zwei Koordinaten miteinander
+	/**
+	 * Vergleicht zwei Koorinaten miteinander
+	 * @param Koorinatenpaar a bestehend aus zwei Doubles
+	 * @param Koorinatenpaar b bestehend aus zwei Doubles
+	 * @return Booleschen Wert und ob die beiden Koordinaten gleich oder ungleich sind
+	 */
 	public boolean compare(GeoPos a, GeoPos b) {
 		if(a == b) return true;
 		else return false;
