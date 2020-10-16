a8b3236941c10a9992394843f673d3f1245616f8
jbalzer12 <61976072+jbalzer12@users.noreply.github.com>
2020-10-16 10:04:38 +0200

    demo

--- src/main/java/org/lakedetection/Main.java
+++ src/main/java/org/lakedetection/Main.java
@@ -15,17 +15,20 @@ public class Main {
 
   		// Initialisieren der Objekts "findsets", indem die Informationen zur gefundenen Datei 
   		// gespeichert werden, um die File zu initialisieren und herunterzuladen.
-    	//FindSets findsets = new FindSets();
+    	FindSets findsets = new FindSets();
     	//File file = findsets.downloadZip(findsets.search());
-    	//System.out.println("search done"); // Kommentar auf der Konsole zur Überprüfung des Fortschritts 
+    	findsets.search();
+    	// ZUM TESTEN OHNE DOWNLOAD
+    	File file = new File("/Users/josefinabalzer/Desktop/softwarepraktikum2020/S1B_IW_GRDH_1SDV_20201015T052435_20201015T052500_023819_02D439_34BD.zip");
+    	System.out.println("search done"); // Kommentar auf der Konsole zur Überprüfung des Fortschritts 
 
     	// Laden des Datensatzes als Objekt vom Typ Loadzip und dem Namen dataset
-    	//Loadzip dataset = new Loadzip(file.getAbsolutePath());
+    	Loadzip dataset = new Loadzip(file.getAbsolutePath());
     	
     	// Pfade zur Test-Datei auf beiden PCs zur Ueberpruefung von Methoden und Funktionen
     	// ohne diese Datei neu herunterladen zu muessen:
     	// Pfad Josi:
-    	Loadzip dataset = new Loadzip("/Users/josefinabalzer/Desktop/softwarepraktikum2020/S1B_IW_GRDH_1SDV_20201015T052435_20201015T052500_023819_02D439_34BD.zip");
+    	//Loadzip dataset = new Loadzip("/Users/josefinabalzer/Desktop/softwarepraktikum2020/S1B_IW_GRDH_1SDV_20201015T052435_20201015T052500_023819_02D439_34BD.zip");
     	// Pfad Alex:
     	//Loadzip dataset = new Loadzip("E:\\Raster\\S1A_IW_GRDH_1SDV_20200307T052505_20200307T052530_031565_03A2FE_508A.zip");
   		
